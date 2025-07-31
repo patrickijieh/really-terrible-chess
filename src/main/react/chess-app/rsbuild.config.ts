abe3176f46f4b0b1721eba9d771d5a68dc472899
Patrick Ijieh <ijiehpatrick@gmail.com>
2025-07-31 19:42:15 -0400

    Add favicon image assets

--- src/main/react/chess-app/rsbuild.config.ts
+++ src/main/react/chess-app/rsbuild.config.ts
@@ -12,5 +12,15 @@ export default defineConfig({
         },
         cleanDistPath: true,
         filenameHash: false,
-    }
+    },
+    html: {
+        favicon: "./src/favicon/favicon.ico",
+        appIcon: {
+            name: "chess",
+            icons: [
+                { src: "./src/favicon/android-chrome-512x512.png", size: 512 },
+                { src: "./src/favicon/android-chrome-192x192.png", size: 192 },
+            ]
+        },
+    },
 });
