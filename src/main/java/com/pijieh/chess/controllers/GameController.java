ed01ab25bf6af3fec31d2122b6e5edb4847b28b5
Patrick Ijieh <ijiehpatrick@gmail.com>
2025-07-30 22:24:31 -0400

    Add basic game and websocket controller and websocket config

---
+++ src/main/java/com/pijieh/chess/controllers/GameController.java
@@ -0,0 +1,48 @@
+package com.pijieh.chess.controllers;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.google.gson.Gson;
+import com.pijieh.chess.business.ChessRoomManager;
+import com.pijieh.chess.business.models.Player;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class GameController {
+
+    @Autowired
+    ChessRoomManager chessRoomManager;
+
+    private static final Gson gson = new Gson();
+
+    @GetMapping("/game")
+    public String game() {
+        return "html/game.html";
+    }
+
+    @MessageMapping("/join/{id}")
+    @SendTo("/game/info")
+    public ResponseEntity<String> playerJoins(@DestinationVariable(value = "id") String gameId,
+            @Payload Player player) {
+
+        if (!chessRoomManager.playerIsInRoom(gameId, player.getName())) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/pijieh/chess/websocket/WebSocketEventListener.java
@@ -0,0 +1,31 @@
+package com.pijieh.chess.websocket;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        log.info("New socket connection");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        log.info("User disconnected");
+    }
+}
